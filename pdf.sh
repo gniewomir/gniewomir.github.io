38cadf177c255a700dd5a0fa679550d5c40aef2a
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2025-07-31 09:37:03 +0200

    spacing fixes in print

--- pdf.sh
+++ pdf.sh
@@ -15,7 +15,7 @@ sleep 10
 if kill -0 $SERVER_PID 2>/dev/null; then
   echo "[PDF] Server is alive under PID $SERVER_PID"
   echo "[PDF] Printing page as PDF..."
-  google-chrome --headless=new --no-pdf-header-footer --no-margins --run-all-compositor-stages-before-draw --timeout=10000 --virtual-time-budget=42000 --print-to-pdf="$FILE" http://localhost:$PORT 1>/dev/null
+  google-chrome --headless=new --no-pdf-header-footer --run-all-compositor-stages-before-draw --timeout=10000 --virtual-time-budget=42000 --print-to-pdf="$FILE" http://localhost:$PORT 1>/dev/null
 else
   echo "[PDF] Server is not present under PID $SERVER_PID"
 fi
